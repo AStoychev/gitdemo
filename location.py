605c96fd00441f3f15181cf7e23b2ed6a43605ae
AStoychev <stoychev.a@abv.bg>
2023-01-17 12:03:27 +0200

    this is demo

---
+++ location.py
@@ -0,0 +1,10 @@
+def town_location(soup):
+    location = soup.find("h1", itemprop="name", class_="main-heading").text
+    current_location = location.split(" ")
+
+    if current_location[2] != " ":
+        loc = current_location[1] + " " + current_location[2]
+    else:
+        loc = current_location[1]
+
+    return loc
